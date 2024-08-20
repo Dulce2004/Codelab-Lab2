2a73072719b2f980e11c81facd80681c219716df
amb231143 <amb231143@uvg.edu.gt>
2024-08-19 19:04:22 -0600

    .
    .

--- app/src/main/java/com/uvg/lemonade/MainActivity.kt
+++ app/src/main/java/com/uvg/lemonade/MainActivity.kt
@@ -10,17 +10,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -58,11 +55,17 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier
         3 -> R.drawable.lemon_drink
         else -> R.drawable.lemon_restart
     }
+    val contentDescription = when (result) {
+        1 -> stringResource(R.string.lemon_tree)
+        2 -> stringResource(R.string.lemon)
+        3 -> stringResource(R.string.glass_of_lemonade)
+        else -> stringResource(R.string.empty_glass)
+    }
     Column (modifier = modifier,
             horizontalAlignment = Alignment.CenterHorizontally){
-        Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random() }) {
+        Image(painter = painterResource(id = imageResource), contentDescription = contentDescription)
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(onClick = { result = (1..4).random() }) {
             Text(stringResource(R.string.roll))
         }
 
